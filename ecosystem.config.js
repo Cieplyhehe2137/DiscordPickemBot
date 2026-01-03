744904a0a9ecf7ef3b537a1ab795b20673556536
Cieplyhehe2137 <lukaszciepichal1@gmail.com>
2026-01-03 19:30:59 +0100

    I don't know what i am doing

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,28 +2,18 @@
 module.exports = {
   apps: [
     {
-      name: "pickembot-hyperland",
+      name: "pickembot",
       cwd: "/home/container",
       script: "index.js",
       env: {
         NODE_ENV: "production",
-        ENV_FILE: "config/hyperland.env",
+        // ważne: nie ENV_FILE
+        // bo teraz guildRegistry sam wczyta wszystkie pliki z folderu config/*.env
+        GUILD_CONFIG_DIR: "config"
       },
       autorestart: true,
       restart_delay: 2000,
       max_restarts: 50,
-    },
-    {
-      name: "pickembot-luffastream",
-      cwd: "/home/container",
-      script: "index.js",
-      env: {
-        NODE_ENV: "production",
-        ENV_FILE: "config/luffastream.env",
-      },
-      autorestart: true,
-      restart_delay: 2000,
-      max_restarts: 50,
-    },
+    }
   ],
 };
