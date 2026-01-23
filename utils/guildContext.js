e7fe5d9b043160295df97779433a1fc5b2f30cd6
Cieplyhehe2137 <lukaszciepichal1@gmail.com>
2026-01-23 19:12:00 +0100

    Awawa

--- utils/guildContext.js
+++ utils/guildContext.js
@@ -1,12 +1,21 @@
-const pool = require('../db');
+// utils/guildContext.js
+const db = require('../db');
 
-async function withGuild(interaction, fn) {
-  const guildId = interaction.guildId;
+async function withGuild(source, fn) {
+  // source może być: interaction ALBO string guildId
+  const guildId =
+    typeof source === 'string'
+      ? source
+      : source?.guildId;
+
   if (!guildId) {
     throw new Error('Brak guildId (DM albo błąd interakcji)');
   }
 
+  const pool = db.getPoolForGuild(guildId);
   return fn(pool, guildId);
 }
 
-module.exports = { withGuild };
+module.exports = {
+  withGuild,
+};
