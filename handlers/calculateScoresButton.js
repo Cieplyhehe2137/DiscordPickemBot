a36a9edae9f0994b873a0e21bb83dae32b94ecd1
Cieplyhehe2137 <lukaszciepichal1@gmail.com>
2026-01-23 22:27:10 +0100

    ascsac

--- handlers/calculateScoresButton.js
+++ handlers/calculateScoresButton.js
@@ -1,5 +1,22 @@
+// handlers/calculateScoresButton.js
+const calculateScores = require('./calculateScores');
 const { withGuild } = require('../utils/guildContext');
 
-await withGuild(guildId, async () => {
-  await calculateScores(guildId);
-});
+module.exports = async function calculateScoresButton(interaction, client) {
+  const guildId = interaction.guildId;
+
+  if (!guildId) {
+    throw new Error('calculateScoresButton called without guildId');
+  }
+
+  await withGuild(guildId, async () => {
+    await calculateScores(guildId);
+  });
+
+  if (!interaction.replied && !interaction.deferred) {
+    await interaction.reply({
+      content: '✅ Punkty zostały przeliczone',
+      ephemeral: true,
+    });
+  }
+};
