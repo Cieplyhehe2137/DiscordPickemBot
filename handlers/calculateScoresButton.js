5120ec7f29e7707424d31476a5f91436b90ed1de
Cieplyhehe2137 <lukaszciepichal1@gmail.com>
2026-01-23 22:26:08 +0100

    dawsa

--- handlers/calculateScoresButton.js
+++ handlers/calculateScoresButton.js
@@ -1,10 +1,5 @@
-const calculateScores = require('./calculateScores');
 const { withGuild } = require('../utils/guildContext');
 
-module.exports = async function calculateScoresButton(interaction) {
-  return withGuild(interaction, async ({ guildId }) => {
-    await interaction.deferReply({ ephemeral: true });
-    await calculateScores(guildId);
-    await interaction.editReply('✅ Punkty przeliczone.');
-  });
-};
+await withGuild(guildId, async () => {
+  await calculateScores(guildId);
+});
